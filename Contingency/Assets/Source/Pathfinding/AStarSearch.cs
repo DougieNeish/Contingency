ebdf6ca1f2b806770d9967c322fd0d9f373e4d92
DougieNeish <douglas@neish.org.uk>
2016-01-25 12:26:42 +0000

    Cleaned up A* search

--- Contingency/Assets/Source/Pathfinding/AStarSearch.cs
+++ Contingency/Assets/Source/Pathfinding/AStarSearch.cs
@@ -17,6 +17,8 @@ public class AStarSearch
 
 	public AStarSearch(int nodeCount)
 	{
+		// Storing node count here could cause issues if number of nodes changes
+		// during the game, depending on how long the AStarSearch object lasts
 		m_nodeCount = nodeCount;
 		m_openList = new HashSet<GraphNode>();
 		m_closedList = new HashSet<GraphNode>();
@@ -34,13 +36,11 @@ public class AStarSearch
 
 		for (int i = 0; i < m_nodeCount; i++)
 		{
-			//m_openList[i] = GraphNode.kInvalidIndex;
-			//m_closedList[i] = GraphNode.kInvalidIndex;
 			m_runningCost[i] = 0f;
 		}
 
 		// TODO: Move into above for loop if m_nodeCount == graph.Nodes.Length (which it should)?
-		// Replace with data structure in A star class instead of nodes to allow concurrent searches
+		// Replace parenting with data structure in A star class instead stored locally to nodes to allow concurrent searches?
 		for (int i = 0; i < graph.Nodes.Length; i++)
 		{
 			graph.Nodes[i].Parent = null;
@@ -52,18 +52,17 @@ public class AStarSearch
 		// Add start node to running cost
 		m_runningCost[startNode.Index] = 0f; // not required due to above for loop init?
 
-		// Add target node to the open list
+		// Add start node to the open list
 		m_openList.Add(startNode);
 
 		GraphNode currentNode;
-		int lastOpenListIndex = 0;
-		int lastClosedListIndex = 0;
 
 		while (m_openList.Count > 0)
 		{
 			Profiler.BeginSample("Find lowest cost in open list");
 			GraphNode bestNode = null;
 			float lowestCost = float.MaxValue;
+
 			foreach (GraphNode tempNode in m_openList)
 			{
 				if (m_runningCost[tempNode.Index] < lowestCost)
@@ -84,14 +83,6 @@ public class AStarSearch
 				return GetPathFromParents(startNode, targetNode);
 			}
 
-			// Add current node to the first empty slot in the closed list
-			//for (int i = 0; i < m_closedList.Length; i++)
-			//{
-			//	if (m_closedList[i] == null)
-			//	{
-			//		m_closedList[i] = currentNode;
-			//	}
-			//}
 			m_closedList.Add(currentNode);
 
 			GraphNode nextNode;
@@ -116,24 +107,30 @@ public class AStarSearch
 				cost = m_runningCost[currentNode.Index] + edge.Cost;
 
 				// If in open list and cost is less than running cost to neighbour, remove from open list because new path is better
-				Profiler.BeginSample("Check open list contains node");
-				if (m_openList.Contains(currentNode) && cost < m_runningCost[nextNode.Index])
-				{
-					Profiler.BeginSample("Remove node from open list");
-					m_openList.Remove(currentNode);
-					Profiler.EndSample();
-				}
-				Profiler.EndSample();
+				//Profiler.BeginSample("Check open list contains node");
+				//if (m_openList.Contains(currentNode) && cost < m_runningCost[nextNode.Index])
+				//{
+				//	Profiler.BeginSample("Remove node from open list");
+				//	m_openList.Remove(currentNode);
+				//	Profiler.EndSample();
+				//}
+				//Profiler.EndSample();
 
 				// If in closed list and cost is less than running cost to neighbour, remove from closed list because new path is better
-				if (m_closedList.Contains(currentNode) && cost < m_runningCost[nextNode.Index])
+				//if (m_closedList.Contains(currentNode) && cost < m_runningCost[nextNode.Index])
+				//{
+				//	m_closedList.Remove(currentNode);
+				//}
+
+				if (m_closedList.Contains(nextNode) && m_runningCost[currentNode.Index] >= m_runningCost[nextNode.Index])
 				{
-					m_closedList.Remove(currentNode);
+					continue;
 				}
 
 				// If not in open or closed list
 				Profiler.BeginSample("Not in open or closed list");
-				if (!m_closedList.Contains(nextNode) && !m_openList.Contains(nextNode) && (m_runningCost[nextNode.Index] == 0f || cost < m_runningCost[nextNode.Index]))
+				//if (!m_closedList.Contains(nextNode) && !m_openList.Contains(nextNode) && (m_runningCost[nextNode.Index] == 0f || cost < m_runningCost[nextNode.Index]))
+				if (!m_openList.Contains(nextNode) || m_runningCost[currentNode.Index] < m_runningCost[nextNode.Index])
 				{
 					// f = g + h
 					m_runningCost[nextNode.Index] = cost + DiagonalDistance(nextNode.Position, targetNode.Position);
@@ -143,38 +140,10 @@ public class AStarSearch
 					nextNode.Parent = currentNode;
 				}
 				Profiler.EndSample();
-
-				// Temporary for profiling
-				//Profiler.BeginSample("Not in closed list");
-				//bool inClosedList = m_closedList.Contains(nextNode);
-				//Profiler.EndSample();
-
-				//Profiler.BeginSample("Not in open list");
-				//bool inOpenList = m_openList.Contains(nextNode);
-				//Profiler.EndSample();
-
-				//Profiler.BeginSample("running cost = 0");
-				//bool costIsZero = m_runningCost[nextNode.Index] == 0f;
-				//Profiler.EndSample();
-
-				//Profiler.BeginSample("running cost < previous");
-				//bool costLessThanPrevious = cost < m_runningCost[nextNode.Index];
-				//Profiler.EndSample();
-
-				//if (!inClosedList && !inOpenList && (costIsZero || costLessThanPrevious))
-				//{
-
-				//	m_runningCost[nextNode.Index] = cost; // + HEURISTIC
-				//	Profiler.BeginSample("Add node to open list");
-				//	m_openList.Add(nextNode);
-				//	Profiler.EndSample();
-				//	nextNode.Parent = currentNode;
-				//}
 			}
 			Profiler.EndSample();
 		}
 
-		// Adding a return above if target node found may have broken profiling? Might not stop at end of A* search?
 		Profiler.EndSample();
 		Debug.Log("<color=red>Target not node found</color>");
 
@@ -248,41 +217,4 @@ public class AStarSearch
 
 		return m_nodePath.ToArray();
 	}
-
-	//private bool IsNodeInArray(GraphNode[] nodeArray, GraphNode node)
-	//{
-	//	for (int i = 0; i < nodeArray.Length; i++)
-	//	{
-	//		if (nodeArray[i] == node)
-	//		{
-	//			return true;
-	//		}
-	//	}
-
-	//	return false;
-	//}
-
-	//private GraphNode GetLowestCost(GraphNode[] nodeArray)
-	//{
-	//	float lowestCost = float.MaxValue;
-	//	for (int i = 0; i < nodeArray.Length; i++)
-	//	{
-	//		if (nodeArray[i].)
-	//	}
-
-	//	return lowestCost;
-	//}
-
-	//private GraphNode GetFirstValidItem(GraphNode[] array)
-	//{
-	//	for (int i = 0; i < array.Length; i++)
-	//	{
-	//		if (array[i] != null)
-	//		{
-	//			return array[i];
-	//		}
-	//	}
-
-	//	return null;
-	//}
 }
