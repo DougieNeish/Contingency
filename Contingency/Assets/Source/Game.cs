ec6ea4061cbbf945f647f30bd5c89cae2b3f5e2b
DougieNeish <douglas@neish.org.uk>
2016-01-26 19:53:11 +0000

    Added player init to Game, Made Game a MonoBehaviour

--- Contingency/Assets/Source/Game.cs
+++ Contingency/Assets/Source/Game.cs
@@ -1,17 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Game
-{	
+public class Game : MonoBehaviour
+{
+	private static int m_nextPlayerID;
+
+	private const int kMaxHumanPlayers = 1;
+	private const int kMaxAIPlayers = 1;
+
+	private Player[] m_players;
+	[SerializeField] GameObject m_humanPlayerPrefab;
+	[SerializeField] GameObject m_AIPlayerPrefab;
+
+	public static int NextPlayerID
+	{
+		get { return m_nextPlayerID++; }
+	}
+
+	public int MaxPlayers
+	{
+		get { return kMaxHumanPlayers + kMaxAIPlayers; }
+	}
+
 	void Awake()
 	{
+		m_nextPlayerID = 0;
+		m_players = new Player[MaxPlayers];
 	}
 
 	void Start()
 	{
-	}
-	
-	void Update()
-	{
+		GameObject player;
+		for (int i = 0; i < kMaxHumanPlayers; i++)
+		{
+			player = Instantiate(m_humanPlayerPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+			player.transform.SetParent(gameObject.transform);
+		}
+
+		for (int i = 0; i < kMaxAIPlayers; i++)
+		{
+			player = Instantiate(m_AIPlayerPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+			player.transform.SetParent(gameObject.transform);
+		}
 	}
 }
