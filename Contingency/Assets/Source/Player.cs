842b4e02c915c2b27a6caf9507b104afb470dfd4
DougieNeish <douglas@neish.org.uk>
2016-01-26 19:51:42 +0000

    Added Player class

---
+++ Contingency/Assets/Source/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	public enum PlayerType
+	{
+		Human,
+		AI,
+	}
+
+	private int m_id;
+	[SerializeField] private PlayerType m_type;
+
+	public int ID
+	{
+		get { return m_id; }
+	}
+
+	public PlayerType Type
+	{
+		get { return m_type; }
+	}
+
+	void Awake()
+	{		
+
+	}
+}
