c734e5904f6c112fb57b9e7cd35a9124de61269e
DougieNeish <douglas@neish.org.uk>
2016-01-28 13:19:01 +0000

    Added PathUIController logic

--- Contingency/Assets/Source/PathUIController.cs
+++ Contingency/Assets/Source/PathUIController.cs
@@ -1,9 +1,58 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class PathUIController : MonoBehaviour
 {
+	private Unit m_unit;
+	private SteeringController m_steeringController;
+	private Path m_path;
+	private LineRenderer m_line;
+
+	[SerializeField] private float m_lineWidth;
+
 	void Awake()
 	{
+		m_unit = GetComponentInParent<Unit>();
+		m_steeringController = GetComponentInParent<SteeringController>();
+		m_path = m_steeringController.PathFollowing.Path;
+		m_line = GetComponent<LineRenderer>();
+	}
+
+	void OnEnable()
+	{
+		m_path.OnWaypointAdded += CalculatePathMarker;
+		m_path.OnCurrentWaypointUpdated += CalculatePathMarker;
+	}
+
+	void OnDisable()
+	{
+		m_path.OnWaypointAdded -= CalculatePathMarker;
+		m_path.OnCurrentWaypointUpdated -= CalculatePathMarker;
+	}
+
+	private void CalculatePathMarker(List<Vector3> waypoints, int currentWaypoint)
+	{
+		// If there are no waypoints, or if the player is an AI don't draw path
+		if (waypoints.Count < 1 || m_unit.Owner.Type == Player.PlayerType.AI)
+		{
+			m_line.enabled = false;
+			return;
+		}
+
+		int remainingWaypoints = waypoints.Count - currentWaypoint;
+		m_line.SetVertexCount(remainingWaypoints + 1);
+		m_line.SetWidth(m_lineWidth, m_lineWidth);
+		m_line.useWorldSpace = true;
+
+		// Add line from unit position to first waypoint
+		int vertexIndex = 0;
+		m_line.SetPosition(vertexIndex++, transform.position);
+
+		for (int i = currentWaypoint; i < waypoints.Count; i++)
+		{
+			m_line.SetPosition(vertexIndex++, waypoints[i]);
+		}
+
+		m_line.enabled = true;
 	}
 }
