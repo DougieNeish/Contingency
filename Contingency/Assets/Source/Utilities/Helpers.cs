f8a9219f88a679db1c7da51e60891dcc39175ee9
DougieNeish <douglas@neish.org.uk>
2016-01-28 01:35:12 +0000

    Added GetComponentInChildWithTag helper function

---
+++ Contingency/Assets/Source/Utilities/Helpers.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Helpers
+{
+	public static T GetComponentInChildWithTag<T>(this GameObject parent, string tag) where T : Component
+	{
+		Transform parentTransform = parent.transform;
+		foreach (Transform child in parentTransform)
+		{
+			if (child.tag == tag)
+			{
+				return child.GetComponent<T>();
+			}
+		}
+
+		return null;
+	}
+}
