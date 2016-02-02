eeea663bec81185d51c0a8d2ab70d1fc300a6e46
DougieNeish <douglas@neish.org.uk>
2016-02-02 15:23:43 +0000

    Added LineOfSightRenderer, Added sight radius to Unit

---
+++ Contingency/Assets/Source/LineOfSightRenderer.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineOfSightRenderer : MonoBehaviour
+{
+	private Player m_owner;
+	private Renderer m_renderer;
+
+	private int m_seenByUnits;
+
+	void Awake()
+	{
+		m_owner = gameObject.GetComponentInParent<Unit>().Owner;
+		m_renderer = gameObject.transform.parent.GetComponent<Renderer>();
+
+		m_seenByUnits = 0;
+	}
+
+	void Start()
+	{
+		Physics.IgnoreCollision(GetComponent<SphereCollider>(), GameObject.FindGameObjectWithTag("Terrain").GetComponent<Collider>());
+	}
+
+	void Update()
+	{
+		m_owner = gameObject.GetComponentInParent<Unit>().Owner;
+		if (m_owner.Type == Player.PlayerType.AI)
+		{
+			if (m_seenByUnits == 0)
+			{
+				m_renderer.enabled = false;
+			}
+			else
+			{
+				m_renderer.enabled = true;
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "SightRadius")
+		{
+			m_seenByUnits++;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "SightRadius")
+		{
+			m_seenByUnits--;
+		}
+	}
+}
--- Contingency/Assets/Source/Units/Unit.cs
+++ Contingency/Assets/Source/Units/Unit.cs
@@ -3,7 +3,11 @@
 public class Unit : MonoBehaviour
 {
 	private int m_id;
-	private Player m_owner;
+	[SerializeField] private Player m_owner;
+
+	[SerializeField] private float m_sightRadius;
+
+	private Renderer m_renderer;
 
 	public int ID
 	{
@@ -16,9 +20,26 @@ public class Unit : MonoBehaviour
 		set { m_owner = value; }
 	}
 
+	public float SightRadius
+	{
+		get { return m_sightRadius; }
+	}
+
 	void Awake()
 	{
 		m_id = UnitController.NextUnitID;
+		m_renderer = GetComponent<Renderer>();
+	}
+
+	void Start()
+	{
+		gameObject.GetComponentInChildWithTag<SphereCollider>("SightRadius").radius = m_sightRadius;
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.cyan;
+		Gizmos.DrawWireSphere(transform.position, m_sightRadius);
 	}
 
 	public static explicit operator GameObject(Unit unit)
