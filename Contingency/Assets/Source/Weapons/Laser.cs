4914a81122afddfc9906593f1df31ac66ddf7b0b
DougieNeish <douglas@neish.org.uk>
2016-02-10 03:17:37 +0000

    Added Weapon base class, Added Laser weapon, Rearranged prefab folder structure

---
+++ Contingency/Assets/Source/Weapons/Laser.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Laser : Weapon
+{
+	private LineRenderer m_laser;
+
+	void Awake()
+	{
+		m_laser = GetComponent<LineRenderer>();
+	}
+	
+	void Start()
+	{
+		m_laser.enabled = false;
+		m_laser.SetWidth(0.1f, 0.3f);
+		m_laser.SetVertexCount(2);
+		m_laser.useWorldSpace = true;
+	}
+
+	public override IEnumerator Fire(IDamageable target)
+	{
+		while (target.Health > 0)
+		{
+			m_laser.SetPosition(0, transform.position);
+			m_laser.SetPosition(1, target.transform.position);
+
+			StartCoroutine(Pulse());
+			target.TakeDamage(Damage);
+
+			yield return new WaitForSeconds(FireRate);
+		}
+	}
+
+	private IEnumerator Pulse()
+	{
+		m_laser.enabled = true;
+		yield return new WaitForSeconds(0.4f);
+		m_laser.enabled = false;
+	}
+}
---
+++ Contingency/Assets/Source/Weapons/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Weapon : MonoBehaviour
+{
+	[SerializeField] private float m_damage;
+	[SerializeField] private float m_range;
+	[SerializeField] private float m_fireRate;
+
+	public float Damage
+	{
+		get { return m_damage; }
+	}
+
+	public float Range
+	{
+		get { return m_range; }
+	}
+
+	public float FireRate
+	{
+		get { return m_fireRate; }
+	}
+
+	public abstract IEnumerator Fire(IDamageable target);
+}
