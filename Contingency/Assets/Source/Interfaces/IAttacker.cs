19048d309e03cfb98d717b338fdf3636874f931e
DougieNeish <douglas@neish.org.uk>
2016-02-10 03:17:56 +0000

    Removed weapon attributes from IAttacker

--- Contingency/Assets/Source/Interfaces/IAttacker.cs
+++ Contingency/Assets/Source/Interfaces/IAttacker.cs
@@ -1,12 +1,7 @@
-﻿using UnityEngine;
-using System.Collections;
-
+﻿
 public interface IAttacker
 {
 	IDamageable CurrentTarget {	get; }
-	float AttackRange {	get; }
-	float AttackDamage { get; }
-	float AttackSpeed { get; }
 
 	void Attack(IDamageable target);
 }
