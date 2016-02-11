29235be8d74cd3640a5a44a42baafc899195f587
DougieNeish <douglas@neish.org.uk>
2016-02-11 02:17:05 +0000

    Added FSM State base class and StateMachine controller

---
+++ Contingency/Assets/Source/FSM/State.cs
@@ -0,0 +1,7 @@
+﻿
+public abstract class State<EntityType>
+{
+	public abstract void Enter(EntityType entity);
+	public abstract void Execute(EntityType entity);
+	public abstract void Exit(EntityType entity);
+}
---
+++ Contingency/Assets/Source/FSM/StateMachine.cs
@@ -0,0 +1,51 @@
+﻿
+public class StateMachine<EntityType>
+{
+	private EntityType m_owner;
+	private State<EntityType> m_currentState;
+	private State<EntityType> m_previousState;
+	private State<EntityType> m_globalState;
+
+	public StateMachine(EntityType owner)
+	{
+		m_owner = owner;
+		m_currentState = null;
+		m_previousState = null;
+		m_globalState = null;
+	}
+
+	public void Update()
+	{
+		if (m_currentState != null)
+		{
+			m_currentState.Execute(m_owner);
+		}
+
+		if (m_globalState != null)
+		{
+			m_globalState.Execute(m_owner);
+		}
+	}
+
+	public void ChangeState(State<EntityType> newState)
+	{
+		if (m_currentState != null)
+		{
+			m_previousState = m_currentState;
+			m_currentState.Exit(m_owner); 
+		}
+
+		m_currentState = newState;
+		m_currentState.Enter(m_owner);
+	}
+
+	public void RevertToPreviousState()
+	{
+		ChangeState(m_previousState);
+	}
+
+	public bool IsInState(State<EntityType> state)
+	{
+		return state.GetType() == m_currentState.GetType();
+	}
+}
