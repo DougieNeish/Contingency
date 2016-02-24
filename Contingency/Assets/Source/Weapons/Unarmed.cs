7194922c6341299cb7937f68933ab31f319a8184
DougieNeish <douglas@neish.org.uk>
2016-02-24 00:14:01 +0000

    Added Unarmed and SniperLaser weapons, Added Scout unit type

---
+++ Contingency/Assets/Source/Weapons/Unarmed.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Unarmed : Weapon
+{
+	public override IEnumerator Fire(IDamageable target)
+	{
+		yield return 1;
+	}
+}
