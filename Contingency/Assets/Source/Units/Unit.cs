0f6381c15837d3bba02009933afac709e9611119
DougieNeish <douglas@neish.org.uk>
2016-03-01 02:02:33 +0000

    Reduced unit defensive range

--- Contingency/Assets/Source/Units/Unit.cs
+++ Contingency/Assets/Source/Units/Unit.cs
@@ -32,7 +32,7 @@ public class Unit : MonoBehaviour, IDamageable, IAttacker
 	private StateMachine<Unit> m_stateMachine;
 	private Vector3 m_lastStationaryPosition;
 
-	public const float kDefensiveRange = 18f;
+	public const float kDefensiveRange = 12f;
 
 	#region Unit Properties
 	public int ID
