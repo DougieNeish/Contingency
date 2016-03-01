3ff4fd3ddf5aa5a913fbd921296447f0e42117ff
DougieNeish <douglas@neish.org.uk>
2016-03-01 02:02:21 +0000

    Units in a defensive stance will not move out of their defensive range when moving to attack

--- Contingency/Assets/Source/Units/State/MovingToAttack.cs
+++ Contingency/Assets/Source/Units/State/MovingToAttack.cs
@@ -1,18 +1,22 @@
-﻿
+﻿using UnityEngine;
+
 public class MovingToAttack : State<Unit>
 {
 	public override void Enter(Unit entity)
 	{
-
 	}
 
 	public override void Execute(Unit entity)
 	{
-
+		// TODO: Stop distance check being called every frame. Move to coroutine?
+		if (entity.Stance == Unit.CombatStance.Defensive &&
+			Vector3.Distance(entity.transform.position, entity.LastStationaryPosition) > Unit.kDefensiveRange)
+		{
+			entity.UnitController.MoveToPosition(entity, entity.LastStationaryPosition);
+		}
 	}
 
 	public override void Exit(Unit entity)
 	{
-
 	}
 }
