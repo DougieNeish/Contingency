e26e5cf32e921740a73003da05fbdd8c3f98bb01
DougieNeish <douglas@neish.org.uk>
2016-04-13 22:30:33 +0100

    Added UnitTest class, Added class diagram

---
+++ Contingency/Assets/Source/UnitTest.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitTest : MonoBehaviour
+{
+	private PathfindingController m_pathfindingController;
+	private const int kTestIterations = 50;
+
+	void Awake()
+    {
+		m_pathfindingController = GameObject.FindGameObjectWithTag("GameManager").GetComponent<PathfindingController>();
+	}
+
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.T))
+		{
+			for (int i = 0; i < kTestIterations; i++)
+			{
+				ExecutePathfindingTest();
+			}
+		}
+	}
+
+	private void ExecutePathfindingTest()
+	{
+		Graph navGraph = m_pathfindingController.NavGraph;
+		Vector3 start = GetRandomNode(navGraph).Position;
+		Vector3 target = GetRandomNode(navGraph).Position;
+
+		Debug.Log("Searching from " + start.ToString() + " to " + target.ToString());
+
+		m_pathfindingController.Search(start, target);
+	}
+	
+	private GraphNode GetRandomNode(Graph graph)
+	{
+		return graph.Nodes[Random.Range(0, graph.Nodes.Length)];
+	}
+}
